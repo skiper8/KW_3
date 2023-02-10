d3fa4a2b3ecd7673288240a62fe41ec1d7889478
askip <a.skiper2013@yandex.ru>
2023-02-10 22:42:08 +0300

    add full programs

--- main/main.py
+++ main/main.py
@@ -1,6 +1,15 @@
-from utils.func import load_operations
+from utils.func import load_operations, date_format, check_format, sort_list
 
-exec_operations = load_operations()
+list_operations = load_operations()
 
-print(exec_operations)
+sort_operations = sort_list(list_operations)
 
+date_format(sort_operations)
+
+check_format(sort_operations)
+
+for item in sort_operations:
+    print(item['date'], item['description'])
+    print(f"{item['from']} -> {item['to']}")
+    print(item['operationAmount']['amount'], item['operationAmount']['currency']['name'])
+    print('')
