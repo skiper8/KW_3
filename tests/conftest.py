67d0d0e93c51e4b67f0d618409a8f46714152cbf
askip <a.skiper2013@yandex.ru>
2023-02-12 01:15:40 +0300

    Переделаны и улучшены тесты

---
+++ tests/conftest.py
@@ -0,0 +1,77 @@
+import pytest
+import requests
+
+"""
+Тут мы обозначаем данные, которыми будут пользоваться тесты
+
+1) Нет поля "form"
+2) "state" == "CANCELED"
+3) Все ОК
+
+Именно эти данные будут использовать тесты, прокидывая их в параметры тестируемых функций
+"""
+
+
+@pytest.fixture()
+def test_data():
+    return [
+        {
+            "id": 863064926,
+            "state": "EXECUTED",
+            "date": "2019-12-08T22:46:21.935582",
+            "operationAmount": {
+                "amount": "41096.24",
+                "currency": {
+                    "name": "USD",
+                    "code": "USD"
+                }
+            },
+            "description": "Открытие вклада",
+            "to": "Счет 90424923579946435907"
+        },
+        {
+            "id": 594226727,
+            "state": "CANCELED",
+            "date": "2018-09-12T21:27:25.241689",
+            "operationAmount": {
+                "amount": "67314.70",
+                "currency": {
+                    "name": "руб.",
+                    "code": "RUB"
+                }
+            },
+            "description": "Перевод организации",
+            "from": "Visa Platinum 1246377376343588",
+            "to": "Счет 14211924144426031657"
+        },
+        {
+            "id": 147815167,
+            "state": "EXECUTED",
+            "date": "2018-01-26T15:40:13.413061",
+            "operationAmount": {
+                "amount": "50870.71",
+                "currency": {
+                    "name": "руб.",
+                    "code": "RUB"
+                }
+            },
+            "description": "Перевод с карты на счет",
+            "from": "Maestro 4598300720424501",
+            "to": "Счет 43597928997568165086"
+        },
+        {
+            "id": 147815167,
+            "state": "EXECUTED",
+            "date": "2018-01-21T15:40:13.413061",
+            "operationAmount": {
+                "amount": "50870.71",
+                "currency": {
+                    "name": "руб.",
+                    "code": "RUB"
+                }
+            },
+            "description": "Перевод с карты на счет",
+            "from": "Счет 43597928997568165086",
+            "to": "Счет 43597928997568165086"
+        }
+    ]
